7cd25b150b0774c3a6a05f6f18fc000da0848b5f
Rosun GV <gvrosun@gmail.com>
2021-04-16 10:38:55 +0530

    Optimized Stacks.py

--- Stacks.py
+++ Stacks.py
@@ -17,57 +17,48 @@ class Stack:
     def push(self, value):
         new_node = Node(value)
         if self.length == 0:
-            self.bottom = Node(value)
-            self.top = self.bottom
+            self.bottom = new_node
+            self.top = new_node
         else:
-            temp = Node(value)
-            self.top.next = temp
-            self.top = temp
+            new_node.next = self.top
+            self.top = new_node
         self.length += 1
         return True
 
     def pop(self):
         if not self.top:
             return None
-        elif self.top == self.bottom:
-            pop_item = self.top.value
-            self.top = None
-            self.bottom = None
-            self.length -= 1
-            return pop_item
-        else:
-            current_node = self.bottom
-            while True:
-                if not current_node.next.next:
-                    pop_item = current_node.next.value
-                    self.top = current_node
-                    self.top.next = None
-                    break
-                current_node = current_node.next
-            self.length -= 1
-            return pop_item
+        pop_item = self.top.value
+        self.top = self.top.next
+        self.length -= 1
+        return pop_item
 
     def isempty(self):
         return self.top is None
 
     def __repr__(self):
-        if not self.bottom:
-            return str(None)
         stack_items = []
-        current_node = self.bottom
-        while current_node.next:
+        current_node = self.top
+        while current_node:
             stack_items.append(current_node.value)
+            if not current_node.next:
+                break
             current_node = current_node.next
-        stack_items.append(current_node.value)
         return str(stack_items)
 
     def __len__(self):
         return self.length
 
 
-my_stack = Stack()
-my_stack.push('google')
-my_stack.push('facebook')
-my_stack.push('udemy')
-my_stack.push('xiaomi')
-print(my_stack, len(my_stack))
+if __name__ == "__main__":
+    my_stack = Stack()
+    my_stack.push('google')
+    my_stack.push('facebook')
+    my_stack.push('udemy')
+    my_stack.push('xiaomi')
+    my_stack.pop()
+    my_stack.pop()
+    my_stack.pop()
+    my_stack.pop()
+    my_stack.push(100)
+    print(my_stack, len(my_stack))
